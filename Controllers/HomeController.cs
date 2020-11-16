7c84b6a6de5acd96f2bc0e819825c050b49a91e3
Jason Summers <3616919+JasonS-Dev@users.noreply.github.com>
2020-11-16 10:27:41 +0000

    Add Backend Feature Flags

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,20 +6,44 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using DotNetDay_201119.Models;
+using Microsoft.FeatureManagement;
 
 namespace DotNetDay_201119.Controllers
 {
     public class HomeController : Controller
     {
+        // using IFeatureManager could cause the 'IsEnabled' status of a feature to change during the course of a request
+        //private readonly IFeatureManager _featureManager;
+
+        // using IFeatureManagerSnapshot preserves the 'IsEnabled' status of a feature during the course of a request
+        private readonly IFeatureManagerSnapshot _featureManager;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IFeatureManagerSnapshot featureManager)
         {
             _logger = logger;
+            _featureManager = featureManager;
         }
 
         public IActionResult Index()
         {
+            ViewData["OutputString"] = "Output from le controller: ";
+
+            if (_featureManager.IsEnabledAsync("FeatureA").Result)
+            {
+                ViewData["OutputString"] += " | Feature A is Enabled!";
+            }
+
+            if (_featureManager.IsEnabledAsync("FeatureB").Result)
+            {
+                ViewData["OutputString"] += " | Feature B is Enabled!";
+            }
+
+            if (_featureManager.IsEnabledAsync("FeatureC").Result)
+            {
+                ViewData["OutputString"] += " | Feature C is Enabled!";
+            }
+
             return View();
         }
 
