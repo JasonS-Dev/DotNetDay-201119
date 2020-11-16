1a414487d84d9ff536a9d657d228bf9dc462e403
Jason Summers <3616919+JasonS-Dev@users.noreply.github.com>
2020-11-16 09:46:37 +0000

    Configure Feature Flags

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.FeatureManagement;
+using Microsoft.FeatureManagement.FeatureFilters;
 
 namespace DotNetDay_201119
 {
@@ -24,6 +26,8 @@ namespace DotNetDay_201119
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddFeatureManagement()
+                .AddFeatureFilter<PercentageFilter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
